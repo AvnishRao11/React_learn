06274ea89f7472d29d2bd90223600ba889ea48d3
AvnishRao11 <avnishrao1122@gmail.com>
2025-11-03 08:01:40 +0530

    functions completed

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const App = () => {
+  const onScroll=(val)=>{
+    console.log("scrolling",val);
+  }
   return (
     <div>
-      <h1>hello guys</h1>
+      <div onWheel={function(elem){
+        onScroll(elem.deltaY);
+      }} className="page1"></div>
+      <div className="page2"></div>
+      <div className="page3"></div>
     </div>
   )
 }
