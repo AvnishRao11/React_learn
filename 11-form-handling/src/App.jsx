63eb33c4bcee4441d7d4128b6d5bb63c2539714c
AvnishRao11 <avnishrao1122@gmail.com>
2025-11-04 07:27:44 +0530

    form-handling

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  const submithandler=(elem)=>{
+    console.log(elem); 
+    elem.preventDefault();
+    console.log("form submitted");
+  }
+  return (
+    <div id='parent'> 
+      <form id="mad" onSubmit={(elem)=>{
+        submithandler(elem);
+      }}>
+      <input  type="text"  placeholder='Enter Your name '/>
+      <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
